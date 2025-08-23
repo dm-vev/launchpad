bd095d1c814ab3f5fa12169100a175ac02eab68f
dm-vev <carmakdoom@gmail.com>
2025-08-23 16:23:52 +0300

    add vtty

--- main/init.c
+++ main/init.c
@@ -3,7 +3,11 @@
 #include "elf/esp_elf.h"
 #include "platform.h"
 
-void launchpad_init(void)
-{
+void launchpad_init_tty(void) {
+    static UARTTTYDriver uartDriver(1, 38, 48, 115200);
+    static TTY tty(&uartDriver);
+}
+
+void launchpad_init(void) {
     _register_symbol("launchpad_platform", (void *)launchpad_platform);
 }
