2da549dcf4bd96c89626c431f85d60cfa54105fd
dm-vev <80632449+dm-vev@users.noreply.github.com>
2025-08-23 19:27:13 +0300

    Register vTTY symbols

--- main/init.c
+++ main/init.c
@@ -7,5 +7,27 @@
 void launchpad_init(void)
 {
     launchpad_vtty_init();
+
+    /* Register vTTY symbols for dynamic lookup */
+    _register_symbol("launchpad_vtty_init", (void *)launchpad_vtty_init);
+    _register_symbol("launchpad_vtty_deinit", (void *)launchpad_vtty_deinit);
+    _register_symbol("launchpad_vtty_register_driver", (void *)launchpad_vtty_register_driver);
+    _register_symbol("launchpad_vtty_set_default", (void *)launchpad_vtty_set_default);
+    _register_symbol("launchpad_vtty_get_current", (void *)launchpad_vtty_get_current);
+    _register_symbol("launchpad_vtty_list", (void *)launchpad_vtty_list);
+    _register_symbol("launchpad_vtty_putc", (void *)launchpad_vtty_putc);
+    _register_symbol("launchpad_vtty_putchar", (void *)launchpad_vtty_putchar);
+    _register_symbol("launchpad_vtty_puts", (void *)launchpad_vtty_puts);
+    _register_symbol("launchpad_vtty_printf", (void *)launchpad_vtty_printf);
+    _register_symbol("launchpad_vtty_flush", (void *)launchpad_vtty_flush);
+    _register_symbol("launchpad_vtty_getc", (void *)launchpad_vtty_getc);
+    _register_symbol("launchpad_vtty_available", (void *)launchpad_vtty_available);
+    _register_symbol("launchpad_vtty_clear_screen", (void *)launchpad_vtty_clear_screen);
+    _register_symbol("launchpad_vtty_move_cursor", (void *)launchpad_vtty_move_cursor);
+    _register_symbol("launchpad_vtty_set_baudrate", (void *)launchpad_vtty_set_baudrate);
+    _register_symbol("launchpad_vtty_is_ready", (void *)launchpad_vtty_is_ready);
+    _register_symbol("launchpad_vtty_set_callback", (void *)launchpad_vtty_set_callback);
+    _register_symbol("launchpad_vtty_ioctl", (void *)launchpad_vtty_ioctl);
+
     _register_symbol("launchpad_platform", (void *)launchpad_platform);
 }
